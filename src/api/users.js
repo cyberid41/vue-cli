5441a56d299388e4b066afbbe674d8fa723c9275
Edi Santoso <edi@kata.ai>
2019-04-23 21:12:13 +0700

    feat: add abstraction api

---
+++ src/api/users.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default {
+    all() {
+        return axios.get('localhost:8000/api/users');
+    },
+    create(data) {
+        return axios.post(`/api/users`, data);
+    },
+    find(id) {
+        return axios.get(`/api/users/${id}`);
+    },
+    update(id, data) {
+        return axios.put(`/api/users/${id}`, data);
+    },
+    delete(id) {
+        return axios.delete(`/api/users/${id}`);
+    },
+};
